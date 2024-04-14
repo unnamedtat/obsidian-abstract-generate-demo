10de77e22b21754bc1f492ca1810aea26380cfbb
unnamedtat <2641167724@qq.com>
2024-04-14 11:22:38 +0800

    change

--- ObsidianAbstractProcess/example/apps.py
+++ ObsidianAbstractProcess/example/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ProcessConfig(AppConfig):
+class ExampleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'process'
+    name = 'example'
