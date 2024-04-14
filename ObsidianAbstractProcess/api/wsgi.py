e3412172cd6c123f683ffe08ff36d8985d2c9ca0
unnamedtat <2641167724@qq.com>
2024-04-14 15:06:51 +0800

    test

--- ObsidianAbstractProcess/api/wsgi.py
+++ ObsidianAbstractProcess/api/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from api.getToken import get_access_token
+from process.getToken import get_access_token
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
---
+++ ObsidianAbstractProcess/process/getAbstract.py
@@ -0,0 +1,15 @@
+
+# def ask_Q(question):
+#     url = "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/completions?access_token=" + get_access_token()
+
+#     payload = json.dumps({
+#         "messages": [
+#             {
+#                 "role": "user",
+#                 "content": question
+#             }
+#         ]
+#     })
+#     headers = {
+#         'Content-Type': 'application/json'
+#     }
