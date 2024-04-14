f3aa8c73377bfa194ab5e77136904dab945fd495
unnamedtat <2641167724@qq.com>
2024-04-14 15:16:49 +0800

    fix bug

--- ObsidianAbstractProcess/process/getToken.py
+++ ObsidianAbstractProcess/process/getToken.py
@@ -1,6 +1,13 @@
 import requests
-from process.urlVariable import Bot_url
 
+def Bot_url(
+    api_key: str,
+    secret_key: str
+    )-> str:
+    '''
+    combine API Key and Secret Key with api url
+    '''
+    return f"https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={api_key}&client_secret={secret_key}"
 
 def get_access_token(
     api_key: str,
@@ -15,6 +22,6 @@ def get_access_token(
         'Accept': 'application/json'
     }
 
-    response = requests.request("POST", Bot_url, headers=headers)
+    response = requests.request("POST", Bot_url(api_key,secret_key), headers=headers)
     return response.json().get("access_token")
 
--- ObsidianAbstractProcess/process/urlVariable.py
+++
@@ -1,1 +0,0 @@
-Bot_url = f"https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={api_key}&client_secret={secret_key}"
