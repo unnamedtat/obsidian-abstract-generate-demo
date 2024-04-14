099256c1e1e8dfeb153dba9d06522185f2c9e154
unnamedtat <2641167724@qq.com>
2024-04-14 15:43:58 +0800

    delete

--- ObsidianAbstractProcess/process/getAbstract.py
+++ ObsidianAbstractProcess/process/getAbstract.py
@@ -1,21 +1,21 @@
-import os
-import json
-import requests
+# import os
+# import json
+# import requests
 
-# users can select the model and promots they want to use
-def ask_Q(mdContent):
-    url = "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/completions?access_token=" + os.environ['ACCESS_TOKEN']
+# # users can select the model and promots they want to use
+# def ask_Q(mdContent):
+#     url = "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/completions?access_token=" + os.environ['ACCESS_TOKEN']
 
-    payload = json.dumps({
-        "messages": [
-            {
-                "role": "user",
-                "content": "你好，我在测试我的功能。"
-            }
-        ]
-    })
-    headers = {
-        'Content-Type': 'application/json'
-    }
-    response = requests.request("POST", url, headers=headers, data=payload)
-    return response.text
+#     payload = json.dumps({
+#         "messages": [
+#             {
+#                 "role": "user",
+#                 "content": "你好，我在测试我的功能。"
+#             }
+#         ]
+#     })
+#     headers = {
+#         'Content-Type': 'application/json'
+#     }
+#     response = requests.request("POST", url, headers=headers, data=payload)
+#     return response.text
